aaa592d6219019411f27e6e13ca05a6bb1e6d201
Scott Feichter <scottfeichter@gmail.com>
2024-06-04 14:42:35 -0700

    misc

--- Module-4-Resources/W10/D2/02-express-error-handling-404/server/app.js
+++ Module-4-Resources/W10/D2/02-express-error-handling-404/server/app.js
@@ -1,8 +1,28 @@
 const express = require('express');
 const app = express();
+// app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('GET / This is the root URL');
+});
+
+app.use((req, res, next) => {
+  const err = new Error();
+  err.message = "Sorry, the requested resource coun't be found.";
+  err.statusCode = "404"
+  // console.log(err);
+  next(err);
+});
+
+app.use('/*', (error, req, res, next) => {
+  // console.log(error);
+  const status = error.statusCode || "500";
+
+  res.status(status)
+  res.json({
+    message: error.message || "Something wrong",
+    status
+  })
 });
 
 const port = 5000;
