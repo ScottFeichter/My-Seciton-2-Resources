f3e340a60ba183cb4c159eb79c5d8bc7d230d7f3
Scott Feichter <scottfeichter@gmail.com>
2024-06-17 13:29:23 -0700

    done with 01 i think

--- Module-4-Resources/W12/D1/01-express-static-files/server/app.js
+++ Module-4-Resources/W12/D1/01-express-static-files/server/app.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const app = express();
 
-// Your code here 
+// Your code here
+
+// app.use('/stylesheets', express.static("assets/css"));
+// app.use(express.static("assets"));
+app.use(express.static("assets/scripts"));
+
+// app.get('stylesheets/application.css', (req, res) => {
+
+// });
 
 const port = 5000;
 app.listen(port, () => console.log('Server is listening on port', port));
